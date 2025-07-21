8da0adac4b91632dc59f81e572af7add2a8e9f0e
trong_khuong <nguyen_trong_khuong@allgrow-labo.jp>
2025-07-21 17:26:15 +0700

    top page

--- _src/js/home.js
+++ _src/js/home.js
@@ -3,13 +3,50 @@ document.addEventListener('DOMContentLoaded', function () {
   const buttons = document.querySelectorAll('.tab_button');
   const contents = document.querySelectorAll('.tab_content');
 
-  buttons.forEach(button => {
+  buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
       buttons.forEach(btn => btn.classList.remove('active'));
       contents.forEach(content => content.classList.remove('active'));
+      document.querySelectorAll('.navi_under').forEach(menu => menu.classList.remove('active'));
 
+      // Display corresponding content tab
       button.classList.add('active');
       document.getElementById(button.dataset.tab).classList.add('active');
+
+      // Show the corresponding fixed menu
+      const naviId = `navi${index + 1}`;
+      const navi = document.getElementById(naviId);
+      if (navi) {
+        navi.classList.add('active');
+      }
     });
+  });
+});
+
+// Animation 
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".js-anima-up");
+  let delayIndex = 0;
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("js-anima-up-on");
+          // observer.unobserve(entry.target); // Bỏ comment nếu chỉ muốn chạy 1 lần
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  elements.forEach((el) => {
+    if (el.classList.contains("js-anima-delay")) {
+      el.style.transitionDelay = `${delayIndex * 0.1}s`;
+      delayIndex++;
+    }
+    observer.observe(el);
   });
 });
